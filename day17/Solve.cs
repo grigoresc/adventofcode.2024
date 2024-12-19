59b9f1071dc590b21f0c8e0ed7670c2b61eff4dc
Ciprian Grigore <grigoresc@outlook.com>
2024-12-19 18:48:56 +0200

    day 17

--- day17/Solve.cs
+++ day17/Solve.cs
@@ -11,12 +11,51 @@ namespace day17
         [InlineData("input.txt", "3,4,3,1,7,6,5,6,0")]
         public void Part1(string input, string sln)
         {
+            var (regA, regB, regC, instructions) = Parse(input);
+            var (output, _) = Run(instructions, regA, regB, regC);
+            var ret = string.Join(",", output);
+            ret.Dump().AssertSolved(sln);
+        }
+
+        [Theory]
+        [InlineData("sample2.txt", 117440)]
+        [InlineData("input.txt", 109019930331546)]
+        public void Part2(string input, long sln)
+        {
+            var (_, _, _, instructions) = Parse(input);
+
+            var aSln = new List<long>();
+            aSln.Add(0);
+
+            for (var pos = instructions.Length - 1; pos >= 0; pos--)
+            {
+                var aLst = aSln.ToArray();
+                aSln.Clear();
+                foreach (var a in aLst)
+                    for (var remainder = 0; remainder < 8; remainder++)
+                    {
+                        var tA = a * 8 + remainder;
+                        var (output, _) = Run(instructions, tA, 0, 0);
+                        if (instructions[pos..].SequenceEqual(output))
+                        {
+                            aSln.Add(tA);
+                        }
+                    }
+            }
+            aSln.Min().Dump().AssertSolved(sln);
+
+        }
+        private static (long regA, long regB, long regC, long[] instructions) Parse(string input)
+        {
             var program = input.ParseAsChunkOfLines();
             var registries = program[0].ReadNumbers();
             var (regA, regB, regC) = (registries[0], registries[1], registries[2]);
-            //(regA, regB, regC).Dump();
             var instructions = program[1].ReadNumbers();
-            //instructions.Dump();
+            return (regA, regB, regC, instructions);
+        }
+
+        private (long[], long) Run(long[] instructions, long regA, long regB, long regC)
+        {
             var i = 0;
             var output = new List<long>();
             while (i < instructions.Length - 1)
@@ -28,7 +67,7 @@ namespace day17
                     4 => regA,
                     5 => regB,
                     6 => regC,
-                    _ => throw new Exception($"op not supported - {op}")
+                    _ => 11111111//throw new Exception($"op not supported - {op}")
                 };
 
                 var isjnz = false;
@@ -51,12 +90,22 @@ namespace day17
                     //cdv
                     case 7: regC = regA / pow2(combo); break;
                 }
+                // 2,4, bst 
+                // 1,5, bxl 
+                // 7,5, cdv
+                // 4,5, bxc
+                // 0,3, adv
+                // 1,6, bxl
+                // 5,5, out
+                // 3,0 go to start is a>0 
+
                 if (!isjnz)
                     i += 2;
 
             }
-            var ret = string.Join(",", output);
-            ret.Dump().AssertSolved(sln);
+
+            return (output.ToArray(), regA);
+            //output.ToArray().Dump();// "output:");
         }
 
         private long pow2(long val)
